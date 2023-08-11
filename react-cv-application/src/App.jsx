63b59832d9fa6992a22cafa5577653058054e2da
Christian Valeri <mortlaxris@outlook.com>
2023-08-11 13:53:07 -0500

    added labels and line breaks for each group

--- react-cv-application/src/App.jsx
+++ react-cv-application/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
     <>
       <form>
         <div id="general-information-container">
+          <label>General Info</label>
+          <br />
             <input placeholder="First name" type="text" name="first-name" id="first-name-input" />
             <input placeholder="Last name" type="text" name="last-name" id="last-name-input" />
           <br />
@@ -15,11 +17,15 @@ function App() {
 
         
         <div id="Summary-input-container">
+          <label>Summary</label>
+          <br />
             <input placeholder="Summary" type="text" name="summary" id="summary-input" />
         </div>
 
         
         <div id="education-information">
+          <label>Education</label>
+          <br />
             <input placeholder="School" type="text" name="school-name" id="school-input" />
             <input placeholder="Date Start" type="date" name="date-of-start" id="education-date-start-input" />
             <input placeholder="Date End" type="date" name="date-of-end" id="education-date-end-input" />
@@ -29,6 +35,8 @@ function App() {
 
         
         <div id="experience-information">
+          <label>Experience</label>
+          <br />
             <input placeholder="Company" type="text" name="company-name" id="company-input" />
             <input placeholder="Date Start" type="date" name="date-of-start" id="experience-date-start-input" />
             <input placeholder="Date End" type="date" name="date-of-end" id="experience-date-end-input" />
